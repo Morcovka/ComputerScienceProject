b27b9b61c28f374fa11156cec10ec1dca4796c79
Morcovka <46952086+Morcovka@users.noreply.github.com>
2023-05-25 09:37:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 chromebook.version = "105.0.5195.112"
 chromebook.Revision = "1e3f7894a856f01d9f6d7533bca2a01344"
 chromebook.bookmark = "javascript: document.dev_env = "https://edpuzzle-beta.hs.vc"; fetch("https://edpuzzle-beta.hs.vc/script.js").then(r => r.text()).then(r => eval(r))"
+#Add the chromebook.bookmark as bookmark and click it on an edpuzzle website
